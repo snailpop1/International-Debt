bcee76ac966224028200074e100e5aacd1c632dd
Deepak George <deepakmgsu@gmail.com>
2024-08-24 10:02:15 -0400

    first commit

---
+++ Datasets/create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE international_debt (
+    country_name VARCHAR(100),
+    country_code CHAR(3),
+    indicator_name VARCHAR(255),
+    indicator_code VARCHAR(50),
+    debt DECIMAL(15, 2)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+AUTO_INCREMENT=1 ;
+
+
+
